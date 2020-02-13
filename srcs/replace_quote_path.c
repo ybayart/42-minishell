79a573b47da982eb9803140707ac4cbdcc68f6f3
Raphael COHEN <racohen@e2r3p21.42.fr>
2020-02-13 22:24:42 +0100

    s

--- srcs/replace_quote_path.c
+++ srcs/replace_quote_path.c
@@ -12,26 +12,39 @@
 
 #include "ft_minishell.h"
 
+int		get_size(char *str)
+{
+	int res;
+
+	res = 0;
+	while (*str != '\0' && *str != '$')
+	{
+		res++;
+		str++;
+	}
+	return (res);
+}
+
 char	*get_env(t_list_env **list, char *input)
 {
 	t_list_env	*cur;
 
 	cur = *list;
-	if (ft_strncmp(cur->name, "?", ft_strlen(cur->name)) == 0)
+	if (ft_strcmp(cur->name, "?") == 0)
 	{
 		return (ft_strfjoin(ft_itoa(g_mini->last_exit),
 			input + (ft_strlen(cur->name))));
 	}
 	while (cur)
 	{
-		if (ft_strcmp(cur->name, input) == 0)
+		if (ft_strncmp(cur->name, input, get_size(input)) == 0)
 		{
 			return (ft_strfjoin(ft_strdup(cur->value),
 				input + (ft_strlen(cur->name))));
 		}
 		cur = cur->next;
 	}
-	return (ft_strdup(""));
+	return (ft_strjoin(ft_strdup(""), input + get_size(input)));
 }
 
 char	**replace_quote_path(char **cmd)
@@ -44,11 +57,12 @@ char	**replace_quote_path(char **cmd)
 	{
 		index = -1;
 		while (cmd[i][++index])
-		{
 			if (cmd[i][index] == '$' && cmd[i][index + 1] != '\0')
+			{
 				cmd[i] = ft_strfjoin(ft_substr(cmd[i], 0, index),
 					get_env(&g_mini->env, &cmd[i][index + 1]));
-		}
+				i--;
+			}
 	}
 	return (cmd);
 }
