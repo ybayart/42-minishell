cb5d32fc10ff5546c0546c8baf350ab605142ef3
ybayart <ybayart@student.42.fr>
2020-02-28 16:18:21 +0100

    fork builtins

--- srcs/run_builtins.c
+++ srcs/run_builtins.c
@@ -6,13 +6,13 @@
 /*   By: ybayart <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/16 13:55:56 by ybayart           #+#    #+#             */
-/*   Updated: 2020/02/18 15:24:30 by racohen          ###   ########.fr       */
+/*   Updated: 2020/02/28 16:16:07 by yanyan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_minishell.h"
 
-int	run_builtins(char *cmd, char **argv)
+static void	run_forked(char *cmd, char **argv)
 {
 	if (ft_strcmp(cmd, "cd") == 0)
 		run_cd(argv);
@@ -26,12 +26,29 @@ int	run_builtins(char *cmd, char **argv)
 		run_unset(argv);
 	else if (ft_strcmp(cmd, "env") == 0)
 		run_env();
-	else if (ft_strcmp(cmd, "exit") == 0)
+}
+
+int			run_builtins(char *cmd, char **argv)
+{
+	pid_t	id;
+
+	if (ft_strcmp(cmd, "exit") == 0)
 	{
 		free(cmd);
 		run_exit(argv);
 	}
-	else
-		return (0);
+	if ((id = fork()) == 0)
+	{
+		run_forked(cmd, argv);
+		exit(0);
+	}
+	g_mini->exec++;
+	g_mini->fork = id;
+	if (g_mini->redir == 0)
+	{
+		waitpid(id, &g_mini->last_exit, 0);
+		g_mini->last_exit = WEXITSTATUS(g_mini->last_exit);
+		g_mini->exec--;
+	}
 	return (1);
 }
